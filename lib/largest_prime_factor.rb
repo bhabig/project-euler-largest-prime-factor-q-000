fb0bbc654dfa439642a0ce886513734b3f78dda1
Bob Habig <rfhab2@gmail.com>
2018-08-04 22:01:53 -0400

    Done.

--- lib/largest_prime_factor.rb
+++ lib/largest_prime_factor.rb
@@ -1,1 +1,17 @@
-# Enter your procedural solution here!
+require 'pry'
+
+def largest_prime_factor(limit)
+   potential_factors = (1..Math.sqrt(limit)).to_a.select { |n| limit % n == 0 }
+
+   potential_factors.select { |n| is_prime?(n) }[-1]
+end
+
+def is_prime?(num)
+   if num == 1
+     true
+   elsif num == 2
+     true
+   else
+     !(2..num/2).to_a.any?{ |n| num % n == 0 }
+   end
+end
--- lib/oo_largest_prime_factor.rb
+++ lib/oo_largest_prime_factor.rb
@@ -1,1 +1,21 @@
-# Enter your object-oriented solution here!
+class LargestPrimeFactor
+   attr_reader :potential_factors
+
+   def initialize(num)
+     @potential_factors = (1..Math.sqrt(num)).to_a.select {|n| num % n == 0}
+   end
+
+   def number
+     self.potential_factors.select { |n| self.is_prime?(n) }[-1]
+   end
+
+   def is_prime?(number)
+     if number == 1
+       true
+     elsif number == 2
+       true
+     else
+       !(2..number/2).to_a.any? { |n| number % n == 0 }
+     end
+   end
+end
